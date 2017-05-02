5636ee619122b446a1d89f3e9c8a88ec5ae34854
dlfvente <deboradelafuente@gmail.com>
2017-05-02 13:32:40 +0200

    SECOND COMMIT

--- app/src/main/java/com/example/deboradlf_guillermopd/mobchat2/adapters/AsynctaskTimer.java
+++ app/src/main/java/com/example/deboradlf_guillermopd/mobchat2/adapters/AsynctaskTimer.java
@@ -1,3 +1,17 @@
 package com.example.deboradlf_guillermopd.mobchat2.adapters;
+
+import java.util.Timer;
+import java.util.logging.Handler;
+
 public class AsynctaskTimer {
+    //Variables
+    Timer timer = new Timer();
+    private int interval = 10000; // 10 seconds by default, can be changed later
+    private Handler _handler;
+
+    public  void RESTcreate(){}
+    public void retrieveFromDate(){}
+
+
+
 }
